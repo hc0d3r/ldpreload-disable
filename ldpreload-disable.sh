e8c7e34be53567cc6e39ea494ff103715984d244
MMxM <mmedeiros450@gmail.com>
2016-09-16 15:09:45 -0300

    create ldpreload-disable.sh

---
+++ ldpreload-disable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -lt 1 ];then
+	printf "./ldpreload-disable.sh [ld-linux.so] [64|32]\n"
+	exit 0
+fi
+
+ld_linux="$1"
+[ "$2" == "64" ] && xor='\x48' || xor=''
+
+offset=$((16#$(readelf -s $ld_linux | grep do_preload | awk '{print $2}')))
+printf $xor'\x31\xc0\xc3' | dd conv=notrunc of=$ld_linux bs=1 seek=$offset
+
