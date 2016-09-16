2a5fff0e866d9bc33c4a5ca3d2cfd277029bdca2
Matheus <mmedeiros450@gmail.com>
2016-09-16 18:23:32 -0300

    removing grep, match using awk

--- ldpreload-disable.sh
+++ ldpreload-disable.sh
@@ -8,6 +8,6 @@ fi
 ld_linux="$1"
 [ "$2" == "64" ] && xor='\x48' || xor=''
 
-offset=$((16#$(readelf -s $ld_linux | grep do_preload | awk '{print $2}')))
+offset=$((16#$(readelf -s $ld_linux | awk '/do_preload/ {print $2}')))
 printf $xor'\x31\xc0\xc3' | dd conv=notrunc of=$ld_linux bs=1 seek=$offset
 
